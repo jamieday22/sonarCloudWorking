24b86710bc57ff6869445a33b6a73005c3d6da43
jamieday22 <jamierobertday@hotmail.com>
2022-01-24 16:31:36 +0000

    update

--- src/main/java/com/DayJ/Animal.java
+++ src/main/java/com/DayJ/Animal.java
@@ -6,7 +6,7 @@ public abstract class Animal {
 
 /////////////////////////////////////Attributes//////////////////////////////////////////////
 
-    private String name; 
+    private String name;
 
 
 
